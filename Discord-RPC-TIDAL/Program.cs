2a043e9cb3a17e6a8902c979c8666ec3f2893e4c
Jonas Kaufmann <jokau03@gmail.com>
2021-01-25 12:11:17 +0100

    error handling and proper disposal of objects - a dialog is now presented to the user when an unhandled exception occurs

--- Discord-RPC-TIDAL/Program.cs
+++ Discord-RPC-TIDAL/Program.cs
@@ -1,19 +1,31 @@
 using discord_rpc_tidal.UI;
+using System;
 using System.Windows.Forms;
 
 namespace discord_rpc_tidal
 {
     static class Program
     {
-        private static readonly MyNotifyIcon MyNotifyIcon = new MyNotifyIcon();
-        private static readonly MyListener MyListener = new MyListener();
+        private static MyNotifyIcon MyNotifyIcon;
+        private static MyListener MyListener;
 
         static void Main()
         {
-            MyNotifyIcon.PropertyChanged += MyNotifyIcon_PropertyChanged;
-            MyNotifyIcon_PropertyChanged(null, new System.ComponentModel.PropertyChangedEventArgs(nameof(MyNotifyIcon.Active))); // start Listener if in status 'active'
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
-            Application.Run();
+            using (MyNotifyIcon = new MyNotifyIcon())
+            using (MyListener = new MyListener())
+            {
+                MyNotifyIcon.PropertyChanged += MyNotifyIcon_PropertyChanged;
+                MyNotifyIcon_PropertyChanged(null, new System.ComponentModel.PropertyChangedEventArgs(nameof(MyNotifyIcon.Active))); // start Listener if in status 'active'
+
+                Application.Run();
+            }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString(), AppDomain.CurrentDomain.FriendlyName, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private static void MyNotifyIcon_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
