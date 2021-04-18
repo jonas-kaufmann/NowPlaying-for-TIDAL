e924d2dbc03418d53d489a109c1be441154ea6bb
Jonas Kaufmann <jokau03@gmail.com>
2021-04-18 14:13:14 +0200

    improved tidal query results, version bump to 1.3.1 - TidalApi: it is now made sure that a song's title matches the input as close as possible; this prevents songs from the same artist containing the same word as the input to be returned - TidalApi: if an artist has published a track in an standalone album and together with other tracks in another album, the track within the album containing the most tracks is chosen - These changes will make sure that the limited number of Discord Assets is more efficiently used - NotifyIcon: Display Version String

--- NowPlaying-for-TIDAL/UI/NotifyIcon.cs
+++ NowPlaying-for-TIDAL/UI/NotifyIcon.cs
@@ -13,17 +13,16 @@ namespace nowplaying_for_tidal.UI
         #region observable properties
 
         private bool active;
+
         public bool Active
         {
             get => active;
             set
             {
-                if (value != active)
-                {
-                    active = value;
-                    ToggleActiveItem.Checked = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Active)));
-                }
+                if (value == active) return;
+                active = value;
+                ToggleActiveItem.Checked = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Active)));
             }
         }
 
@@ -32,8 +31,6 @@ namespace nowplaying_for_tidal.UI
 
         private readonly NotifyIcon NotifyIcon = new NotifyIcon();
         private readonly ToolStripMenuItem ToggleActiveItem;
-        private readonly ToolStripItem ConfigItem;
-        private readonly ToolStripItem ExitItem;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -55,11 +52,21 @@ namespace nowplaying_for_tidal.UI
             ToggleActiveItem.CheckedChanged += (sender, args) => Active = ToggleActiveItem.Checked;
             cms.Items.Add(ToggleActiveItem);
 
-            ConfigItem = cms.Items.Add(ConfigText);
-            ConfigItem.Click += (sender, args) => Process.Start("explorer.exe", $"/select,\"{AppConfig.ConfigPath}\"");
+            var configItem = cms.Items.Add(ConfigText);
+            configItem.Click += (sender, args) => Process.Start("explorer.exe", $"/select,\"{AppConfig.ConfigPath}\"");
 
-            ExitItem = cms.Items.Add(ExitText);
-            ExitItem.Click += (sender, args) => Application.Exit();
+            // show app version
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            if (version != null)
+            {
+                var versionString = $"{version.Major}.{version.Minor}.{version.Build}";
+
+                cms.Items.Add(new ToolStripSeparator());
+                cms.Items.Add(new ToolStripLabel(versionString) {Enabled = false});
+            }
+
+            var exitItem = cms.Items.Add(ExitText);
+            exitItem.Click += (sender, args) => Application.Exit();
 
             NotifyIcon.ContextMenuStrip = cms;
 
--- NowPlaying-for-TIDAL/Utils/TidalApi.cs
+++ NowPlaying-for-TIDAL/Utils/TidalApi.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using SimpleTidalApi;
@@ -39,9 +41,50 @@ namespace nowplaying_for_tidal.Utils
                 if (searchResult.Item2?.Tracks == null || searchResult.Item2.Tracks.Count == 0)
                     return null;
 
-                // Find correct track with matching artists in case of other songs having the same name, tracks are sorted by relevancy so the first match is usually correct.
+                // Find correct track
                 var artistsText = artists.Replace(", ", @" / ");
-                return searchResult.Item2.Tracks.FirstOrDefault(t => t.ArtistsName == artistsText);
+                var tracksWithCorrectArtists =
+                    searchResult.Item2.Tracks.Where(t => t.ArtistsName == artistsText); // match artist names
+                var tracksWithCorrectName =
+                    tracksWithCorrectArtists.Where(t => t.DisplayTitle.StartsWith(title)).OrderBy(t => t.DisplayTitle.Length).ToList(); // match display title
+
+                // take songs with display title closest to input
+                var firstTrack = tracksWithCorrectName.FirstOrDefault();
+                if (firstTrack == null)
+                    return null;
+                tracksWithCorrectName = tracksWithCorrectName.Where(t => t.DisplayTitle.Length == firstTrack.DisplayTitle.Length).ToList(); // eliminate all tracks that have a longer name than the shortest
+                if (tracksWithCorrectName.Count == 1)
+                    return tracksWithCorrectName.FirstOrDefault();
+                
+                // Find the track within an album with the most tracks
+                // Query API for album information
+                var albums = new Dictionary<long, Album>();
+                Parallel.ForEach(tracksWithCorrectName, track =>
+                {
+                    var task = TidalClient.Search(loginKey, track.Album.Title + " " + track.Artists[0].Name, eType: QueryFilter.ALBUM);
+                    task.Wait();
+                    var (item1, item2) = task.Result;
+                    if (item1 != null || item2.Albums.Count == 0)
+                        return;
+
+                    var correspondingAlbum = item2.Albums.FirstOrDefault(album => album.ID == track.Album.ID);
+                    if (correspondingAlbum == null) return;
+                    lock (albums)
+                    {
+                        albums.Add(correspondingAlbum.ID, correspondingAlbum);
+                    }
+                });
+                
+                Console.WriteLine(albums.Count);
+
+                foreach (var track in tracksWithCorrectName.Where(track => albums.ContainsKey(track.Album.ID)))
+                {
+                    track.Album = albums[track.Album.ID];
+                }
+                
+                // return title within the album with the most tracks
+                return tracksWithCorrectName.OrderByDescending(t => t.Album.NumberOfTracks).FirstOrDefault();
+
             });
         }
     }
